21df1f25b839e7101292458b5874833b4445350c
Mauricio Carmona <mauriciomtzcarmona@gmail.com>
2019-10-10 21:34:58 -0500

    Fixed variables and duplicated code.

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 // grab data from https://memegen.link/examples ->  data = 'string'
 
-var https = require('https');
+const https = require('https');
 let htmlOfWebsite;
-let htmlMatch;
-let htmlSlicedToTen;
-let htmlCorrectLink;
-let htmlWithHttps;
+const htmlMatch;
+const htmlSlicedToTen;
+const htmlCorrectLink;
+const htmlWithHttps;
 const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
 
-var fs = require('fs');
-var dir = './top_memes';
+const fs = require('fs');
+const dir = './top_memes';
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
 
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
-}
-
-var options = {
+const options = {
   host: 'memegen.link',
   path: '/examples'
 };
-var request = https.request(options, function(res) {
+const request = https.request(options, function(res) {
   htmlOfWebsite = '';
   res.on('data', function(chunk) {
     htmlOfWebsite += chunk;
@@ -40,7 +36,7 @@ request.end();
 function main(htmlOfWebsite) {
   htmlMatch = htmlOfWebsite.match(/"\/.+watermark=none" style/g);
 
-  let htmlDecoded = htmlMatch.map(urlDecoded => entities.decode(urlDecoded));
+  const htmlDecoded = htmlMatch.map(urlDecoded => entities.decode(urlDecoded));
 
   // let htmlFiltered = htmlMatch.filter(function htmtFiltering(filter) {
   // return !filter.includes('&#');
@@ -56,11 +52,11 @@ function main(htmlOfWebsite) {
 
   console.log(htmlWithHttps);
 
-  for (i = 0; i < 10; i++) {
-    const fs = require('fs');
+  for (let i = 0; i < 10; i++) {
 
     const file = fs.createWriteStream(`top_memes/file${i}.jpg`);
 
+    //requestTwo is never used!
     const requestTwo = https.get(htmlWithHttps[i], function(response) {
       response.pipe(file);
     });
